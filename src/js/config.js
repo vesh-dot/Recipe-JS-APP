2fd619d602f9dd8607575c9564c3eb45ee83601e
cesh <veshrajb30@gmail.com>
2021-10-07 18:03:13 +1100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b497993d-51e6-4718-9488-6e8f3e3100a3';
+export const MODAL_CLOSE_SEC = 2.5;
